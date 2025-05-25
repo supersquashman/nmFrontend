348bb049b1ead2d3ab40e4a5e15b3dfc41a791e1
supersquashman <supersquashman@gmail.com>
2025-05-24 21:10:46 -0500

    small change

--- src/app/nm-map.service.ts
+++ src/app/nm-map.service.ts
@@ -17,9 +17,9 @@ export class NmMapService {
   // Example data, this could be fetched from an API
   private rooms: Room[] = [
     { id: 1, name: "The Ninja Academy Entrance", x: 0, y: 2, exits: { north: 2 }, description: "A giant teardrop of a house sits here boldly" },
-    { id: 2, name: "Path Through the Forst", x: 0, y: 1, exits: { south: 1, north: 3 } },
-    { id: 3, name: "Eastern Edge of the Forest", x: 0, y: 0, exits: { south: 2, southeast:4 } },
-    { id: 4, name: "Deep Forestry", x: 1, y: 1, exits: {northwest: 3, east:5}},
+    { id: 2, name: "Path Through the Forst", x: 0, y: 1, exits: { south: 1, north: 3 }, description: "Just a room" },
+    { id: 3, name: "Eastern Edge of the Forest", x: 0, y: 0, exits: { south: 2, southeast:4 }, description: "Just a room" },
+    { id: 4, name: "Deep Forestry", x: 1, y: 1, exits: {northwest: 3, east:5}, description: "Just a room"}
     // { id: 5, name: "Deep Forestry", x:2, y:1, exits: {west:4}}
   ];
 
